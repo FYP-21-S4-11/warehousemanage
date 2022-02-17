ff8a27a4f6e387393464be09c9481da211d48cf6
joyce-99 <91814421+joyce-99@users.noreply.github.com>
2022-02-17 20:16:19 +0800

    8:16pm

--- application/__init__.py
+++ application/__init__.py
@@ -13,20 +13,16 @@ from sqlalchemy.orm import sessionmaker
 def open_connection():
     unix_socket = '/cloudsql/{}'.format("numeric-asset-341503:us-west4:fypdatabase")
     try:
-        if os.environ.get('GAE_ENV') == 'standard':
-            conn = pymysql.connect(user="root",
-                                   password="root",
-                                   unix_socket=unix_socket,
-                                   db="fypdatabase",
-                                   cursorclass=pymysql.cursors.DictCursor
-                                   )
+        conn = pymysql.connect(user="root",
+                               password="root",
+                               unix_socket=unix_socket,
+                               db="fypdatabase",
+                               cursorclass=pymysql.cursors.DictCursor)
     except pymysql.MySQLError as e:
         return e
     return conn
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1234"
 
-
-from application import admin, supplier, staff, stock, store, product
+from application import user
