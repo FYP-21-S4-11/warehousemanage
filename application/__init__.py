9b1abf78c0d32c8c770ec81369166c2f7177c817
joyce-99 <91814421+joyce-99@users.noreply.github.com>
2022-02-17 16:29:22 +0800

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,28 +1,17 @@
+import mysql.connector
 from flask import Flask
-import pymysql
-import sqlalchemy
-from flask import Flask
-from google.cloud.sql.connector import connector
 
-
-def getconn() -> pymysql.connections.Connection:
-    conn: pymysql.connections.Connection = connector.connect(
-        "project:us-west4:fypdatabase",
-        "mysql+pymysql",
-        user="root",
-        password="root",
-        db="fypdatabase",
-        auth_plugin="mysql_native_password",
-        autocommit=True,
-    )
-    return conn
-mydb = sqlalchemy.create_engine(
-    "mysql+pymysql://",
-    creator=getconn,
-)
+unix_socket = '/cloudsql/{}'.format("us-west4:fypdatabase")
+mydb = mysql.connector.connect(
+    user="root",
+    password="root",
+    database="fypdatabase",
+    unix_socket=unix_socket,
+    auth_plugin="mysql_native_password",
+    autocommit=True)
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1234"
-
 
 from application import admin, supplier, staff, stock, store, user, product
