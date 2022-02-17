cae3da8d660600ee355d98e74c4ce156cbc544ac
joyce-99 <91814421+joyce-99@users.noreply.github.com>
2022-02-17 23:41:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from application import app
 import os
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
