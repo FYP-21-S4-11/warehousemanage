cf8131d0c82a64f73f24b90af5e661d19a11bf3b
joyce-99 <91814421+joyce-99@users.noreply.github.com>
2022-02-17 23:31:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from application import app
 import os
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, debug=True, host="0.0.0.0",  port=8080)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
